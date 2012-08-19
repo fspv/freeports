320189cd0a804a451195f7bb602b9c86ff99620a
Safronov Pavel <pv.safronov@gmail.com>
2012-08-19 19:13:33 +0400

    Измененно дефолтное название базы и имя пользователя

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ DATABASE_STORAGE_ENGINE = 'MYISAM'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'swmap_test',                      # Or path to database file if using sqlite3.
-        'USER': 'swmap_test',                      # Not used with sqlite3.
+        'NAME': 'swmap',                      # Or path to database file if using sqlite3.
+        'USER': 'swmap',                      # Not used with sqlite3.
         'PASSWORD': '12345',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
