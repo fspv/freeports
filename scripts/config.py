f6aaef10c3b9e5f36b0cc5ea94479df7606b392a
Safronov Pavel <pv.safronov@gmail.com>
2012-08-19 19:28:41 +0400

    config.py был сломан гитом.

--- scripts/config.py
+++ scripts/config.py
@@ -4,7 +4,6 @@ swmap_username = "swmap"
 swmap_password = "map123"
 # MySQL config
 mysql_host = "localhost"
-<<<<<<< HEAD
 mysql_user = "swmap_test"
 mysql_pass = "12345"
 mysql_dbname = "swmap_test"
@@ -15,13 +14,3 @@ map_dir = "/home/testprojects/freeports/"
 website_location = "/"
 # Debug
 DEBUG = True
-=======
-mysql_user = "root"
-mysql_pass = "******"
-mysql_dbname = "swmap2"
-# System paths ("/" at the end of path necessary!)
-tmp_dir = "/tmp/swmap/"
-map_dir = "/home/djprogects/freeports/"
-# Website URL
-website_location = "/"
->>>>>>> ed587351429901d440c5f2ddc4c716fff5466c81
