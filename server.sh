36abc9d5501f86c681d9358f59b73ed9c4e41dfe
Safronov Pavel <pv.safronov@gmail.com>
2012-08-19 19:43:33 +0400

    server.sh был сломан гитом.

--- server.sh
+++ server.sh
@@ -2,11 +2,7 @@
 case "$1" in
     "start")
         find /home/djprogects/freeports -name "*.pyc" -exec rm '{}' \; 
-<<<<<<< HEAD
         python2 manage.py runfcgi method=prefork host=127.0.0.1 port=8882 pidfile=/tmp/server.pid 
-=======
-        python2 manage.py runfcgi method=prefork host=127.0.0.1 port=8881 pidfile=/tmp/server.pid 
->>>>>>> ed587351429901d440c5f2ddc4c716fff5466c81
     ;;
     "stop") 
         kill -9 `cat /tmp/server.pid`
