40f8aa5bdee719a82a05b2fe0d0497602cf81385
Pavel Safronov <pv.safronov@gmail.com>
2021-10-18 22:16:27 +0100

    Make static locations accessible without nginx

--- settings.py
+++ settings.py
@@ -1,30 +1,30 @@
+import os
+
 # Django settings for freeports project.
 
 DEBUG = True
-#DEBUG = False
+# DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    ('Safronov Pavel', 'pv.safronov@gmail.com'),
-)
+ADMINS = (("Safronov Pavel", "pv.safronov@gmail.com"),)
 
-INTERNAL_IPS = ('89.249.172.184',)
+INTERNAL_IPS = ("127.0.0.1",)
 
 MANAGERS = ADMINS
-STORAGE_ENGINE = 'MYISAM'
-DATABASE_STORAGE_ENGINE = 'MYISAM'
+STORAGE_ENGINE = "MYISAM"
+DATABASE_STORAGE_ENGINE = "MYISAM"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'swmap',                      # Or path to database file if using sqlite3.
-        'USER': 'swmap',                      # Not used with sqlite3.
-        'PASSWORD': '12345',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'STORAGE_ENGINE': 'MYISAM',
-        'OPTIONS': {
-            'init_command': 'SET storage_engine=MYISAM;',
-        }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        "NAME": "swmap_test",  # Or path to database file if using sqlite3.
+        "USER": "swmap_test",  # Not used with sqlite3.
+        "PASSWORD": "12345",  # Not used with sqlite3.
+        "HOST": "172.17.0.2",  # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",  # Set to empty string for default. Not used with sqlite3.
+        "STORAGE_ENGINE": "MYISAM",
+        "OPTIONS": {
+            "init_command": "SET storage_engine=MYISAM;",
+        },
     }
 }
 
@@ -35,11 +35,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = "Europe/Moscow"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
 SITE_ID = 1
 
@@ -53,27 +53,27 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = ""
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = ""
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/testprojects/freeports/static/'
+STATIC_ROOT = os.getcwd() + "/static/"
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/'
+STATIC_URL = "/static/"
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = "/static/admin/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -85,49 +85,50 @@ STATICFILES_DIRS = (
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '%@0n$$bf&*%7w%0p++euqjm#+o97(_5sry!_99=ds2(th&@q9!'
+SECRET_KEY = "%@0n$$bf&*%7w%0p++euqjm#+o97(_5sry!_99=ds2(th&@q9!"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+    #     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     #'freeports.swmap.middleware.BeautifulSoupMiddleware',
-    'freeports.swmap.middleware.MultipleProxyMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "freeports.swmap.middleware.MultipleProxyMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 )
 
-ROOT_URLCONF = 'freeports.urls'
+ROOT_URLCONF = "freeports.urls"
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/testprojects/freeports/templates',
+    os.getcwd()
+    + "/templates",
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'swmap',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "swmap",
     #'debug_toolbar',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
@@ -141,19 +142,16 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "mail_admins": {"level": "ERROR", "class": "django.utils.log.AdminEmailHandler"}
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
         },
-    }
+    },
 }
